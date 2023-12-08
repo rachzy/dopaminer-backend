25c5f16b8b5d1454f28964e31d168b3ff72ad7d1
Henrique <henriqueferreira.fox@gmail.com>
2023-12-08 14:04:37 -0300

    rename accounts directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AccountModule } from './modules/accounts/account.module';
+import { AccountModule } from './modules/account/account.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { SessionModule } from './modules/session/session.module';
