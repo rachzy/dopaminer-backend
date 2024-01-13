014a2c779104e249e04d714db30ee7c28a7381d6
Henrique <henriqueferreira.fox@gmail.com>
2024-01-12 21:49:59 -0300

    feat: edit task

---
+++ src/modules/task/pipes/create-task/create-task.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { CreateTaskPipe } from './create-task.pipe';
+
+describe('CreateTaskPipe', () => {
+  it('should be defined', () => {
+    expect(new CreateTaskPipe()).toBeDefined();
+  });
+});
---
+++ src/modules/task/pipes/edit-task/edit-task.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { EditTaskPipe } from './edit-task.pipe';
+
+describe('EditTaskPipe', () => {
+  it('should be defined', () => {
+    expect(new EditTaskPipe()).toBeDefined();
+  });
+});
